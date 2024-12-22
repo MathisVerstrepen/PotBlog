62ac25f390cfc6352e7b737833409f1754eb83f7
Mathis Verstrepen <mathisverstrepen@gmail.com>
2024-12-22 09:35:29 -0500

    fix: refactor MarkdownToHTML

--- services/markdown.go
+++ services/markdown.go
@@ -43,12 +43,63 @@ type MarkdownHTML struct {
 	Metadata Metadata
 }
 
+func MarkdownToHTML(md *string) (MarkdownHTML, error) {
+	metadata, err := markdownToMetadata(md)
+	if err != nil {
+		return MarkdownHTML{}, err
+	}
+
+	return MarkdownHTML{
+		RawHTML:  *md,
+		Metadata: metadata,
+	}, nil
+}
+
 var MetadataTagMap = map[string]string{
 	"title":       "Title",
 	"description": "Description",
 	"date":        "Date",
 	"tags":        "Tags",
 	"author":      "Author",
+}
+
+func markdownToMetadata(md *string) (Metadata, error) {
+	metadataBlock, err := extractMetadataBlock(*md)
+	if err != nil {
+		return Metadata{}, err
+	}
+
+	metadataLines := strings.Split(metadataBlock, "\n")
+
+	metadata := Metadata{}
+	for _, line := range metadataLines {
+		mdTag := strings.Split(line, ":")
+		if len(mdTag) < 2 {
+			continue
+		}
+
+		tag, value, err := parseMetadataLine(line)
+		if err != nil {
+			continue
+		}
+
+		if metadataTag, ok := MetadataTagMap[tag]; ok {
+			switch metadataTag {
+			case "Title":
+				metadata.Title = value
+			case "Description":
+				metadata.Description = value
+			case "Date":
+				metadata.Date = value
+			case "Tags":
+				metadata.Tags = parseTags(value)
+			case "Author":
+				metadata.Author = value
+			}
+		}
+	}
+
+	return metadata, nil
 }
 
 func extractMetadataBlock(content string) (string, error) {
@@ -81,56 +132,4 @@ func parseTags(value string) []string {
 		tags = append(tags, strings.TrimSpace(tag))
 	}
 	return tags
-}
-
-func markdownToMetadata(md *string) (Metadata, error) {
-	metadataBlock, err := extractMetadataBlock(*md)
-	if err != nil {
-		return Metadata{}, err
-	}
-
-	metadataLines := strings.Split(metadataBlock, "\n")
-
-	metadata := Metadata{}
-	for _, line := range metadataLines {
-		mdTag := strings.Split(line, ":")
-		if len(mdTag) < 2 {
-			continue
-		}
-
-		tag := strings.TrimSpace(mdTag[0])
-		value := strings.TrimSpace(strings.Join(mdTag[1:], ":"))
-
-		if metadataTag, ok := MetadataTagMap[tag]; ok {
-			switch metadataTag {
-			case "Title":
-				metadata.Title = value
-			case "Description":
-				metadata.Description = value
-			case "Date":
-				metadata.Date = value
-			case "Tags":
-				metadata.Tags = []string{}
-				for _, tag := range strings.Split(value, ",") {
-					metadata.Tags = append(metadata.Tags, strings.TrimSpace(tag))
-				}
-			case "Author":
-				metadata.Author = value
-			}
-		}
-	}
-
-	return metadata, nil
-}
-
-func MarkdownToHTML(md *string) (MarkdownHTML, error) {
-	metadata, err := markdownToMetadata(md)
-	if err != nil {
-		return MarkdownHTML{}, err
-	}
-
-	return MarkdownHTML{
-		RawHTML:  *md,
-		Metadata: metadata,
-	}, nil
 }
