ed74ea1b6311cf444b37dc687a5427254f6820ae
Mathis Verstrepen <mathisverstrepen@gmail.com>
2024-12-28 12:45:42 -0500

    add: Dev articles page header style

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                beige: "#ECDFCC",
-                dark_beige: "#D1BFA8",
-                orange: "#DB7E21",
-                dark_orange: "#BE6D1C",
-                red: "#A23C3C",
-                green: "#697565",
-                dark_green: "#4e564b",
-                grey: "#3C3D37",
-                black: "#181C14",
+                beige: "var(--c_beige)",
+                dark_beige: "var(--c_dark_beige)",
+                orange: "var(--c_orange)",
+                dark_orange: "var(--c_dark_orange)",
+                red: "var(--c_red)",
+                green: "var(--c_green)",
+                dark_green: "var(--c_dark_green)",
+                grey: "var(--c_grey)",
+                black: "var(--c_black)",
             },
         },
         fontFamily: {
