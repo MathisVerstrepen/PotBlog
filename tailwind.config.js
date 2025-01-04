fa7f042b928adfe94dab3109ebaeb9b1b43ad10b
Mathis Verstrepen <mathisverstrepen@gmail.com>
2025-01-03 21:40:17 -0500

    fix: custom color not compatible with tailwind opacity

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,15 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                beige: "var(--c_beige)",
-                dark_beige: "var(--c_dark_beige)",
-                orange: "var(--c_orange)",
-                dark_orange: "var(--c_dark_orange)",
-                red: "var(--c_red)",
-                green: "var(--c_green)",
-                dark_green: "var(--c_dark_green)",
-                grey: "var(--c_grey)",
-                black: "var(--c_black)",
+                beige: 'rgb(var(--c_beige)/<alpha-value>)',
+                dark_beige: 'rgb(var(--c_dark_beige)/<alpha-value>)',
+                orange: 'rgb(var(--c_orange)/<alpha-value>)',
+                dark_orange: 'rgb(var(--c_dark_orange)/<alpha-value>)',
+                red: 'rgb(var(--c_red)/<alpha-value>)',
+                green: 'rgb(var(--c_green)/<alpha-value>)',
+                dark_green: 'rgb(var(--c_dark_green)/<alpha-value>)',
+                grey: 'rgb(var(--c_grey)/<alpha-value>)',
+                black: 'rgb(var(--c_black)/<alpha-value>)',
             },
         },
         fontFamily: {
